5ec04e4529db93d11647c788ae7d8d8a89666d43
l1xnan <l1xnan@qq.com>
2023-08-20 02:40:58 +0800

    support alias config

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,7 +1,13 @@
+import path from "path";
 import { defineConfig } from "vite";
 import renderer from "vite-plugin-electron-renderer";
 
 // https://vitejs.dev/config
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   plugins: [renderer({})],
 });
